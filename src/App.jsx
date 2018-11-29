428e8d716882cd57b750282884204a362efe94fc
Chandon <chandonking@hotmail.com>
2018-11-29 16:21:49 +0000

    changed router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import News from './components/News';
@@ -10,14 +10,14 @@ import Navbar from './components/CustomNavbar';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <div>
           <Navbar />
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/news" component={News} />
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
