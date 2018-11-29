7af2bc722913bf81d927ea12a2ab11a54880e079
Chandon <chandonking@hotmail.com>
2018-11-29 12:09:22 +0000

    Updates

---
+++ precache-manifest.1ef2222462279c9028261f93f58b8471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "d6f432bd56b96d8524ba",
+    "url": "/react-gh-pages/static/js/main.d6f432bd.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-gh-pages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d6f432bd56b96d8524ba",
+    "url": "/react-gh-pages/static/css/main.3533af4a.chunk.css"
+  },
+  {
+    "revision": "c2b8f854afcf7fed83ebae364ba46593",
+    "url": "/react-gh-pages/index.html"
+  }
+];
